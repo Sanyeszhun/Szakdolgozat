616e5a9b4fbe7d5389c878882aa790381956b92f
Szilvási Sándor <sanyeszhun98@gmail.com>
2020-02-16 12:19:32 +0100

    RepositoryKamionDatabasetable

---
+++ Szakdolgozat/Smart Trucks Kft/Smart Trucks Kft/Repository/Kamion/RepositoryKamionDatabaseTable.cs
@@ -0,0 +1,96 @@
+﻿using MySql.Data.MySqlClient;
+using Smart_Trucks_Kft.Modell;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Trucks_Kft.Repository
+{
+    partial class RepositoryKamionDatabaseTable
+    {
+        private readonly string connectionStringCreate;
+        private readonly string connectionString;
+
+
+        public RepositoryKamionDatabaseTable() 
+        {
+
+            ConnectionString cs = new ConnectionString();
+            connectionStringCreate = cs.getCreateString();
+            connectionString = cs.getConnectionString();
+
+
+        }
+
+       
+
+        public void createTableKamion()
+        {
+            string queryUSE = "USE kamion;";
+            string queryCreateTable=
+                "Create TABLE `kamionok` (" +
+                "   `tid` int(60) NOT NULL DEFAULT '0', " +
+                 "`Muszakierv` date NOT NULL DEFAULT '2017-01-01'," +
+                  "   `Rendszam` varchar(60) COLLATE latin2_hungarian_ci NOT NULL DEFAULT '', " +
+                   "   `Motor` varchar(60) COLLATE latin2_hungarian_ci NOT NULL DEFAULT '', " +
+                    "   `Uzemanyag` varchar(60) COLLATE latin2_hungarian_ci NOT NULL DEFAULT '', " +
+                     "   `Suly` int(60) NOT NULL DEFAULT '0', " +
+                       "   `Hajtas` varchar(60) COLLATE latin2_hungarian_ci NOT NULL DEFAULT '', " +
+                        ")ENGINE = InnoDB; ";
+            string queryPrimaryKey =
+                "ALTER TABLE `kamionok`  ADD PRIMARY KEY(`tid`); ";
+          
+            MySqlConnection connection =
+               new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmdUSE = new MySqlCommand(queryUSE, connection);
+                cmdUSE.ExecuteNonQuery();
+                MySqlCommand cmdCreateTable = new MySqlCommand(queryCreateTable, connection);
+                cmdCreateTable.ExecuteNonQuery();
+                MySqlCommand cmdPrimaryKey = new MySqlCommand(queryPrimaryKey, connection);
+                cmdPrimaryKey.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Tábla lérehozása sikertelen.");
+            }
+
+        }
+        /// <summary>
+        /// Kamionok tábla törlése kamion adatbázisból
+        /// </summary>
+
+        public void deleteTableKamion()
+        {
+            string query =
+                "USE kmion; " +
+                "DROP TABLE kmionok;";
+
+            MySqlConnection connection =
+                new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Tábla törlése nem sikerült.");
+            }
+        }
+
+
+    }
+}
---
+++ Szakdolgozat/Smart Trucks Kft/Smart Trucks Kft/Repository/RepositoryDatabase.cs
@@ -0,0 +1,78 @@
+﻿using MySql.Data.MySqlClient;
+using Smart_Trucks_Kft.Modell;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Trucks_Kft.Repository
+{
+    class RepositoryDatabase
+    {
+        private readonly string connectionStringCreate;
+        private readonly string connectionString;
+
+        /// <summary>
+        /// Konstruktor - kezdőértékadások
+        /// </summary>
+        public RepositoryDatabase()
+        {
+            ConnectionString cs = new ConnectionString();
+            connectionStringCreate = cs.getCreateString();
+            connectionString = cs.getConnectionString();
+        }
+
+        /// <summary>
+        /// Létrehozza a "csarp" adatbazist
+        /// </summary>
+
+        public void createDatabase()
+        {
+            string query =
+                "CREATE DATABASE IF NOT EXISTS kamion " +
+                "DEFAULT CHARACTER SET utf8 " +
+                "COLLATE utf8_hungarian_ci ";
+
+            MySqlConnection connection =
+                new MySqlConnection(connectionStringCreate);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Adatbázis létrehozás nem sikerült vagy már létezik.");
+            }
+        }
+
+
+        public void deleteDatabase()
+        {
+            string query =
+                "DROP DATABASE kamion;";
+
+            MySqlConnection connection =
+                new MySqlConnection(connectionString);
+            try
+            {
+                connection.Open();
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                cmd.ExecuteNonQuery();
+                connection.Close();
+            }
+            catch (Exception e)
+            {
+                connection.Close();
+                Debug.WriteLine(e.Message);
+                throw new RepositoryException("Adatbázis törlése nem sikerült.");
+            }
+        }
+    }
+}
