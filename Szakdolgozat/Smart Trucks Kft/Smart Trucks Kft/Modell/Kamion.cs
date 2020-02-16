c2eaeffa80f89c2555798dbead863bd8096f8d83
Szilvási Sándor <sanyeszhun98@gmail.com>
2020-02-16 09:50:09 +0100

    Kamion.cs kész

--- Szakdolgozat/Smart Trucks Kft/Smart Trucks Kft/Modell/Kamion.cs
+++ Szakdolgozat/Smart Trucks Kft/Smart Trucks Kft/Modell/Kamion.cs
@@ -10,7 +10,7 @@ namespace Smart_Trucks_Kft.Modell
     {
 
         private int tid;
-        private int date;//datum jo formatum megadasa
+        private int date; //datum jo formatum megadasa
         private string rendszam;
         private string motor;
         private string uzemanyag;
@@ -20,6 +20,7 @@ namespace Smart_Trucks_Kft.Modell
         public Kamion(int tid, int date, string rendszam, string motor, string uzemanyag, int suly, string hajtas)
         {
             this.tid = tid;
+            this.date = date;
             //datum kell meg this.date
 
             if (!isValideRendszam(rendszam))
@@ -31,14 +32,106 @@ namespace Smart_Trucks_Kft.Modell
             this.motor = motor;
             this.uzemanyag = uzemanyag;
             this.suly = suly;
-            this.hajtas = hajta;
+            this.hajtas = hajtas;
 
 
 
         }
+        public void setTid(int tid) 
+        {
+            this.tid = tid;
+        
+        }
 
+        public void setDate(int date)
+        {
+            this.date = date;
+
+        }
+
+        public void setRend(string rendszam)
+        {
+            this.rendszam = rendszam;
+
+        }
+
+        public void setMotor(string motor) 
+        {
+            this.motor = motor;
+        
+        }
+
+        public void setUzemanyag(string uzemanyag)
+        {
+            this.uzemanyag = uzemanyag;
+
+        }
+
+        public void setSuly(int suly)
+        {
+            this.suly = suly;
+
+        }
+
+        public void setHajtas(string hajtas)
+        {
+            this.hajtas =hajtas;
+
+        }
+
+        public int getId() 
+        {
+            return tid;
+        
+        }
+        public int getDate() 
+        {
+            return date;
+        
+        }
+
+        public string getRend()
+        {
+            return rendszam;
+
+        }
+
+        public string getMotor() 
+        {
+
+            return motor;
+        
+        }
+
+        public string getUzemanyag() 
+        {
+            return uzemanyag;
+        
+        }
+
+        public int getSuly()
+        {
+            return suly;
 
         
+        }
+
+        public string getHajtas()
+        {
+            return hajtas;
+        
+        }
+
+        public void update(Kamion modified)
+        {
+            this.date = modified.getDate();
+            this.rendszam = modified.getRend();
+            this.motor = modified.getMotor();
+            this.uzemanyag = modified.getUzemanyag();
+            this.suly = modified.getSuly();
+            this.hajtas = modified.getHajtas();
+        }
+
 
 
     }
