7913f9aa64e243d1240ecfa6c7892d52e78655c8
Szilvási Sándor <sanyeszhun98@gmail.com>
2020-03-18 17:22:01 +0100

    kikuldes nem jo

---
+++ Szakdolgozat/Smart Trucks Kft/Smart Trucks Kft/Modell/Kuldes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smart_Trucks_Kft.Modell
+{
+    partial class Kikuldes
+    {
+        private int sid;
+        private int kid;
+        private int heid;
+        private int teid;
+        private int did;
+        private string kikuldes;
+        private string visszaerkezes;
+
+
+        public Kikuldes(int sid, int kid, int heid, int teid, int did, string kikuldes, string visszaerkezes)
+        {
+
+            this.sid = sid;
+            this.kid = kid;
+            this.heid = heid;
+            this.teid = teid;
+            this.did = did;
+            this.kikuldes = kikuldes;
+            this.visszaerkezes = visszaerkezes;
+        }
+
+        public int getKikuldesId()
+        {
+            return sid;
+        }
+        public string getFelvetelezohely()
+        {
+            return felvetelezohely;
+        }
+        public string getTavolsag()
+        {
+            return tavolsag;
+        }
+        public string getIdo()
+        {
+            return ido;
+        }
+
+        public string GetLerakohely() 
+        {
+
+            return lerakohely;
+        }
+    
+
+
+}
+}
